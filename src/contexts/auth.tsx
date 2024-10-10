922772bded8e2c12e76d2a4d4325f2e62052ca8b
Vuinishiusu <ewerton.viniciusturco@gmail.com>
2024-10-10 18:28:10 -0300

    feat: add persistência no login

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,5 +1,13 @@
 import * as Google from "expo-auth-session/providers/google";
-import { type ReactNode, createContext, useEffect, useState } from "react";
+import {
+  type ReactNode,
+  createContext,
+  useEffect,
+  useState,
+  useCallback,
+} from "react";
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
 
 type UserType =
   | {
@@ -14,12 +22,15 @@ type AuthContextType = {
   signed: boolean;
   signInWithGoogle: () => Promise<void>;
   signOut: () => void;
+  getToken: () => Promise<void>;
+  loading: boolean;
 };
 
 export const AuthContext = createContext({} as AuthContextType);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<UserType>();
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<UserType>(undefined);
 
   const config = {
     webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
@@ -28,7 +39,27 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   };
   const [_, response, promptAsync] = Google.useAuthRequest(config);
 
-  const getUserProfile = async (token: any) => {
+  const validateToken = async (token: string) => {
+    try {
+      const res = await fetch(
+        `https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${token}`,
+      );
+      if (res.ok) {
+        const data = await res.json();
+        const condition = Platform.select({
+          ios: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
+          android: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
+        });
+        if (data && data.aud === condition) {
+          return true;
+        }
+      }
+    } catch (err) {
+      return false;
+    }
+  };
+
+  const getUserProfile = useCallback(async (token: string | undefined) => {
     if (!token) return;
     try {
       const res = await fetch("https://www.googleapis.com/userinfo/v2/me", {
@@ -40,17 +71,26 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       setUser({ email: user.email, name: user.name, photoUrl: user.picture });
     } catch (err) {
       console.log("error", err);
+    } finally {
+      setLoading(false);
     }
-  };
+  }, []);
 
-  const handleToken = () => {
+  const handleToken = useCallback(async () => {
     if (response?.type === "success") {
       const { authentication } = response;
       const token = authentication?.accessToken;
-      getUserProfile(token);
+      if (token != null) {
+        Promise.all([
+          SecureStore.setItemAsync("userToken", token),
+          getUserProfile(token),
+        ]);
+      }
     }
-  };
+  }, [getUserProfile, response]);
 
+  // "response" nas dependência para que para verificação do token
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     handleToken();
   }, [response]);
@@ -65,11 +105,30 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     }
   };
 
-  const signOut = () => setUser(undefined);
+  const getToken = async () => {
+    const token = await SecureStore.getItemAsync("userToken");
+    if (token != null) {
+      const isValid = await validateToken(token);
+      if (isValid) return await getUserProfile(token);
+    }
+    setLoading(false);
+  };
+
+  const signOut = async () => {
+    await SecureStore.deleteItemAsync("userToken");
+    setUser(undefined);
+  };
 
   return (
     <AuthContext.Provider
-      value={{ signed: Boolean(user), user, signInWithGoogle, signOut }}
+      value={{
+        signed: Boolean(user),
+        user,
+        signInWithGoogle,
+        signOut,
+        loading,
+        getToken,
+      }}
     >
       {children}
     </AuthContext.Provider>
