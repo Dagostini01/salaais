5480b1683ad4b2f21f3eb2b6558572ed7c063693
Vuinishiusu <ewerton.viniciusturco@gmail.com>
2024-10-10 14:24:47 -0300

    feat: add lib axios e stripe

--- App.tsx
+++ App.tsx
@@ -6,19 +6,24 @@ import theme from "./src/global/global/theme";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { AuthProvider } from "./src/contexts/auth";
 import * as WebBrowser from "expo-web-browser";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-      <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <AuthProvider>
-            <AppRoutes />
-          </AuthProvider>
-        </NavigationContainer>
-      </ThemeProvider>
+      <StripeProvider
+        publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+      >
+        <ThemeProvider theme={theme}>
+          <NavigationContainer>
+            <AuthProvider>
+              <AppRoutes />
+            </AuthProvider>
+          </NavigationContainer>
+        </ThemeProvider>
+      </StripeProvider>
     </GestureHandlerRootView>
   );
 }
