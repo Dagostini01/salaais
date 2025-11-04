0d0adc5b7055b1d88508250e6927ccdc183bd0d9
Vinicius <ewerton.viniciusturco@gmail.com>
2025-11-04 20:50:48 -0300

    refactor: progressStatusBar

--- src/components/ProgressStatusBar/styles.ts
+++ src/components/ProgressStatusBar/styles.ts
@@ -3,11 +3,15 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   width: 85%;
   margin-top: 10px;
+  align-self: center;
 `;
 
-export const ProgressBar = styled.View<{ status: "success" | "primary" | "attention" }>`
+export const ProgressBar = styled.View<{
+  status: "success" | "primary" | "attention";
+}>`
   height: 25px;
-  background-color: ${({ theme, status }) => theme.colors[status]};
+  background-color: ${({ theme, status }) =>
+    theme.colors[status as keyof typeof theme.colors]};
   border-radius: 8px;
   justify-content: center;
   align-items: center;
@@ -16,7 +20,4 @@ export const ProgressBar = styled.View<{ status: "success" | "primary" | "attent
 export const ProgressText = styled.Text`
   color: white;
   font-weight: bold;
-  `;
-  //width: 100%;
-  //text-align: center;
-  //border: 1px;
+`;
